dfa923a7aacd72980897169931a17b6221411b3b
maccuci <antoniogabrielleal@gmail.com>
2025-02-27 16:54:11 -0300

    feat: add emoji-based role assignment and update config

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID, CHANNEL_ID, ROLE_ID } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID, CHANNEL_ID, ROLE_ID, EMOJI_ID, SPECIAL_ROLE_ID } = process.env;
 
 if (!DISCORD_CLIENT_ID || !DISCORD_TOKEN || !GUILD_ID) {
     throw new Error('Missing environment variables');
 }
 
-export const config = { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID, CHANNEL_ID, ROLE_ID };
+export const config = { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID, CHANNEL_ID, ROLE_ID, EMOJI_ID, SPECIAL_ROLE_ID };
--- src/index.ts
+++ src/index.ts
@@ -5,53 +5,82 @@ import { PRESENTATION_MESSAGES } from './constats';
 import { deployCommands } from './desploy-commands';
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
 });
 
 (async () => {
-    deployCommands({ guildId: config.GUILD_ID! });
+  deployCommands({ guildId: config.GUILD_ID! });
 })();
 
 client.once('ready', () => {
-    console.log('TutorandusBot is ready 🦾');
+  console.log('TutorandusBot is ready 🦾');
 });
 
 client.on(Events.InteractionCreate, (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
 });
 
 client.on('messageCreate', async (message) => {
-    try {
-        if (message.channel.id === config.CHANNEL_ID && !message.author.bot) {
-            const role = message?.guild?.roles.cache.get(config.ROLE_ID!);
+  try {
+    if (message.channel.id === config.CHANNEL_ID && !message.author.bot) {
+      const role = message?.guild?.roles.cache.get(config.ROLE_ID!);
 
-            if (
-                !message.member?.roles.cache.has(role?.id != undefined ? role.id : '') &&
-                role &&
-                PRESENTATION_MESSAGES.some((item) => message.content.toLowerCase().includes(item))
-            ) {
-                await message.member?.roles.add(role);
-            }
-        }
-    } catch (err) {
-        console.error('Deu erro garaio', err);
+      if (
+        !message.member?.roles.cache.has(role?.id != undefined ? role.id : '') &&
+        role &&
+        PRESENTATION_MESSAGES.some((item) => message.content.toLowerCase().includes(item))
+      ) {
+        await message.member?.roles.add(role);
+      }
     }
+  } catch (err) {
+    console.error('Deu erro garaio', err);
+  }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  if (reaction.emoji.id === config.EMOJI_ID) {
+    const role = reaction.message.guild?.roles.cache.get(config.SPECIAL_ROLE_ID!);
+
+    if (
+      !reaction.message.member?.roles.cache.has(role?.id != undefined ? role.id : '') &&
+      role &&
+      !reaction.message.author?.bot
+    ) {
+      await reaction.message.member?.roles.add(role);
+    }
+  }
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+  if (reaction.emoji.id === config.EMOJI_ID) {
+    const role = reaction.message.guild?.roles.cache.get(config.SPECIAL_ROLE_ID!);
+
+    if (
+      !reaction.message.member?.roles.cache.has(role?.id != undefined ? role.id : '') &&
+      role &&
+      !reaction.message.author?.bot
+    ) {
+      await reaction.message.member?.roles.remove(role);
+    }
+  }
 });
 
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) {
-        return;
-    }
-    const { commandName } = interaction;
-    if (commands[commandName as keyof typeof commands]) {
-        commands[commandName as keyof typeof commands].execute(interaction);
-    }
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName } = interaction;
+  if (commands[commandName as keyof typeof commands]) {
+    commands[commandName as keyof typeof commands].execute(interaction);
+  }
 });
 
 client.login(config.DISCORD_TOKEN);
